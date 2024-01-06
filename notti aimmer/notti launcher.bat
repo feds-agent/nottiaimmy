5476929edc8caa7defddbd9933e11c527fbc0ccd
x5k1 <137819891+x5k1@users.noreply.github.com>
2024-01-06 16:44:05 +0100

    Add files via upload

---
+++ notti aimmer/notti launcher.bat
@@ -0,0 +1,13 @@
+@echo off
+title notti launcher
+
+:main
+echo [1] - exit
+echo  [2] - Aimmy
+set /p X= ;
+if %X%==1 (
+    exit
+)
+if %X%==2 (
+    start C:\Users\48696\Desktop\aimmy\iu9htRkh.exe
+)
